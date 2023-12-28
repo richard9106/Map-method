ea3736f4795fb21da9e0e5ea3d402004d5ffa969
Richard Marles <marleswebinfo@gmail.com>
2023-12-28 09:05:57 +0100

    Adding filter metohd

--- map.js
+++ map.js
@@ -5,16 +5,26 @@
  */
 
 // Using a for loop
-
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums){
+  results.push(num * 2);
+}
+console.log(results);
 // Using map()
+const multByTwo = (num)=>{
+  return num *2;
+}
+let mapResults = nums.map(multByTwo);
 
-
+console.log(mapResults);
 // Simplified w/ map()
-
+let mapResultsSimple1 = nums.map(function(num){return num * 2});
+console.log(mapResultsSimple1);
 
 // Simplfied w/ map() + arrow function
-
+let mapResultsSimple2 = nums.map((num)=>num*2);
+console.log(mapResultsSimple2);
 
 // With objects:
 const students = [
@@ -37,3 +47,8 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map( student =>[{"id" : student.id, "name" : student.name}] );
+console.log(studentsWithIds);
+const studentsWithIds2 = students.map(student => student = students);
+console.log(studentsWithIds2);
